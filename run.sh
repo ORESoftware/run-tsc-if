e6480b392197a4476b4e85b213323389c02b0999
Alexander Mills <alex@alexs-mac.local>
2019-08-20 20:31:51 -0700

    tmp

--- run.sh
+++ run.sh
@@ -2,18 +2,19 @@
 
 run_tsc='nope'
 project_root="${project_root:-$1}"
+root_dir="${root_dir:-src}"
+dist_dir="${dist_dir:-dist}"
+export PATH="$project_root/node_modules/.bin:${PATH}"
 
-PATH="$project_root/node_modules/.bin:${PATH}"
-
-for f in `(cd "$project_root/src" && find . -type f)`; do
+for f in `(cd "$project_root/$root_dir" && find . -type f)`; do
 
     f="${f:2}" # remove first 2 chars
     length="${#f}"
     end_index=$(expr "$length" - '3')
     jsf="${f:0:$end_index}.js"
 
-    full_src="$project_root/src/$f";
-    full_dist="$project_root/dist/$jsf";
+    full_src="$project_root/$root_dir/$f";
+    full_dist="$project_root/$dist_dir/$jsf";
 
     if [[ "$full_src" != *'.ts' ]]; then
       continue;
@@ -35,11 +36,32 @@ for f in `(cd "$project_root/src" && find . -type f)`; do
 
 done
 
-if [[ "$run_tsc" == 'yes' ]]; then
+check_sha(){
+  (
+    cd "$project_root"
+    mkdir -p "node_modules/.sha/run-tsc-if"
+    new_sha="$(sha1sum package.json)"
+    old_sha=$(cat "node_modules/.sha/run-tsc-if/package.json.sha");
+    echo "$new_sha" > "node_modules/.sha/run-tsc-if/package.json.sha"
+    if [[ "$new_sha" != "$old_sha" ]]; then
+       echo 'yes'
+       echo 1;
+    fi
+    exit 0;
+  )
+}
 
-  echo "PATH => $PATH";
+if [[ "$run_tsc" != 'yes' ]]; then
+   if ! check_sha; then
+      run_tsc='yes'
+   fi
+fi
+
+
+if [[ "$run_tsc" == 'yes' ]]; then
 
   echo 'Running transpile with tsc, because we have un-transpiled files.';
 
-  (cd "$project_root" && tsc)
+  (cd "$project_root" && npm i && tsc)
+
 fi
