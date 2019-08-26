113083c687cc5d8cf222e47f33b13ea64f60ec44
Operations Research Engineering Software+ <alex@oresoftware.com>
2019-08-25 19:51:36 -0700

    Update install.sh
    change location to bin folder

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ ql_green='\033[1;32m'
 ql_no_color='\033[0m'
 
 
-mkdir -p "$HOME/.oresoftware/bash"
+mkdir -p "$HOME/.oresoftware/bin"
 
 curl -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/oresoftware/run-tsc-if/master/run.sh?$(date +%s)" \
---output "$HOME/.oresoftware/bash/run-tsc-if.sh"
+--output "$HOME/.oresoftware/bin/run-tsc-if"
 
-chmod u+x "$HOME/.oresoftware/bash/run-tsc-if.sh"
+chmod u+x "$HOME/.oresoftware/bin/run-tsc-if"
 
 echo "";
 echo -e "${ql_green} => run-tsc-if download/installation succeeded.${ql_no_color}";
